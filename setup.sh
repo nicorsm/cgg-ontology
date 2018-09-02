74681f54ad25b0a9af799523b1d6636d883b85d0
Nicola <nicorsm@me.com>
2018-09-02 20:56:44 +0200

    Added base and CGG ontologies

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-java -jar widoco.jar -rewriteAll -confFile ./config/congress.properties -includeImportedOntologies -webVowl -ontURI http://www.cgeg.sm/ontology/congress# -outFolder congress -ontFile rdfs/congress.rdf
-java -jar widoco.jar -rewriteAll -confFile ./config/base.properties -includeImportedOntologies -webVowl -ontURI http://www.cgeg.sm/ontology/base# -outFolder base -ontFile rdfs/base.rdf
-java -jar widoco.jar -rewriteAll -confFile ./config/cgg.properties -includeImportedOntologies -webVowl -ontURI http://www.cgeg.sm/ontology/cgg# -outFolder cgg -ontFile rdfs/cgg.rdf
+java -jar widoco.jar -rewriteAll -confFile ./config/base.properties -includeImportedOntologies -webVowl -ontURI http://www.cgeg.sm/ontology/base# -outFolder ./rdfs/base -ontFile ./rdfs/base.rdf
+java -jar widoco.jar -rewriteAll -confFile ./config/congress.properties -includeImportedOntologies -webVowl -ontURI http://www.cgeg.sm/ontology/congress# -outFolder ./rdfs/congress -ontFile ./rdfs/congress.rdf 
+java -jar widoco.jar -rewriteAll -confFile ./config/cgg.properties -includeImportedOntologies -webVowl -ontURI http://www.cgeg.sm/ontology/cgg# -outFolder ./rdfs/cgg -ontFile ./rdfs/cgg.rdf 
+
 java -jar widoco.jar -rewriteAll -confFile ./config/commissions.properties -includeImportedOntologies -webVowl -ontURI http://www.cgeg.sm/ontology/commissions# -outFolder commissions -ontFile rdfs/commissions.rdf
 java -jar widoco.jar -rewriteAll -confFile ./config/council-of-12.properties -includeImportedOntologies -webVowl -ontURI http://www.cgeg.sm/ontology/council-of-12# -outFolder council-of-12 -ontFile rdfs/council-of-12.rdf
 java -jar widoco.jar -rewriteAll -confFile ./config/counciliar-works.properties -includeImportedOntologies -webVowl -ontURI http://www.cgeg.sm/ontology/counciliar-works# -outFolder counciliar-works -ontFile rdfs/counciliar-works.rdf
